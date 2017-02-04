db60e88674a78d8b85aeab717335c788816e2df3
joelbowen <jmichaelb722@gmail.com>
2017-02-04 15:40:59 -0500

    Basic project scaffolding
    Just getting sorted

---
+++ webpack.config.babel.js
@@ -0,0 +1,69 @@
+/* eslint no-console:"off" */
+const { resolve } = require('path');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpackValidator = require('webpack-validator');
+const { getIfUtils, removeEmpty } = require('webpack-config-utils');
+const OfflinePlugin = require('offline-plugin');
+
+module.exports = env => {
+  const { ifProd, ifNotProd } = getIfUtils(env);
+  const config = {
+    context: resolve('src'),
+    entry: {
+      app: './app.js',
+    },
+    output: {
+      filename: ifProd('bundle.[name].[chunkhash].js', 'bundle.[name].js'),
+      path: resolve('dist'),
+      pathinfo: ifNotProd(),
+    },
+    devtool: ifProd('source-map', 'eval'),
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loaders: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.?s?css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader'],
+          }),
+        },
+        {
+          test: /\.png|.jpg/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    plugins: removeEmpty([
+      new ProgressBarPlugin(),
+      new ExtractTextPlugin(ifProd('styles.[name].[chunkhash].css', 'styles.[name].css')),
+      ifProd(new InlineManifestWebpackPlugin()),
+      ifProd(new webpack.optimize.CommonsChunkPlugin({
+        names: ['manifest'],
+      })),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'head',
+      }),
+      new OfflinePlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: ifProd('"production"', '"development"')
+        }
+      }),
+    ]),
+  }
+  if (env.debug) {
+    console.log(config);
+    debugger; // eslint-disable-line
+  }
+  return config;
+}
